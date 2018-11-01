ea2fa2d224bea7c1b48202ef81634fe079990ff4
HOLYMOSH/holymosh <dpopov1997@gmail.com>
2018-11-01 14:32:10 +0300

    -

--- link_lib_example/link_lib/libMock/libMockPP.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by holymosh on 23.09.2018.
-//
-
-#include "libMockPP.h"
-#include <iostream>
-void libMockPP::helloWorld() {
-    std::cout<<"hello cpp"<<std::endl;
-}
--- link_lib_example/link_lib/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "libMock/libMockPP.h"
-int main() {
-    libMockPP libMockPP1  = libMockPP();
-    libMockPP1.helloWorld();
-    return 0;
-}
